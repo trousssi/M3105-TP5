cf5795314328c2bdb81dc6fc9af12f433cee44a9
vivierlo <vivierlo@pc-dg-033-04>
2016-10-05 11:15:37 +0200

    Interpreteur.h

--- M3105-TP5(g)/Interpreteur.h
+++ M3105-TP5(g)/Interpreteur.h
@@ -32,10 +32,10 @@ private:
     Noeud*  facteur();     //     <facteur> ::= <entier>  |  <variable>  |  - <facteur>  | non <facteur> | ( <expression> )
                            //   <opBinaire> ::= + | - | *  | / | < | > | <= | >= | == | != | et | ou
     Noeud*  instSi();      //      <instSi> ::= si ( <expression> ) <seqInst> finsi
-    Noeud*  instTantQue()  // <instTantQue> ::=tantque( <expression> ) <seqInst> fintantque
+    Noeud*  instTantQue();  // <instTantQue> ::=tantque( <expression> ) <seqInst> fintantque
     
     // outils pour simplifier l'analyse syntaxique
-    void tester (const string & symboleAttendu) const throw (SyntaxeException);   // Si symbole courant != symboleAttendu, on lève une exception
+    void tester(const string & symboleAttendu) const throw (SyntaxeException);   // Si symbole courant != symboleAttendu, on lève une exception
     void testerEtAvancer(const string & symboleAttendu) throw (SyntaxeException); // Si symbole courant != symboleAttendu, on lève une exception, sinon on avance
     void erreur (const string & mess) const throw (SyntaxeException);             // Lève une exception "contenant" le message mess
 };
