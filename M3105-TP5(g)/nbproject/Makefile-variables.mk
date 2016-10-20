9c7df8d5c2414f0a33a18d14fa5a7f876577bb88
vivierlo <vivierlo@pc-dg-033-04>
2016-10-21 01:00:03 +0200

    mise en forme des tests interpréteur

--- M3105-TP5(g)/nbproject/Makefile-variables.mk
+++ M3105-TP5(g)/nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
 CND_ARTIFACT_NAME_Debug=m3105-tp5_g_
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/m3105-tp5_g_
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/m3105-tp5_g_
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
 CND_PACKAGE_NAME_Debug=m3105-tp5(g).tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/m3105-tp5(g).tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/m3105-tp5(g).tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux
 CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
